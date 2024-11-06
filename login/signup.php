3d54eac8a134298647d3ec4658d099ae9d9d6a41
jongitshub <jonbryant93@gmail.com>
2024-11-06 08:33:53 -0500

    Now only php files with html in same file

--- login/signup.php
+++ login/signup.php
@@ -14,3 +14,20 @@
 
     mysqli_close($conn);
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign Up</title>
+</head>
+<body>
+<form method="post">
+    <input style="max-width: 50%;" id="email" type="email" name="email" placeholder="Enter email"><br><br>
+    <input style="max-width: 50%;" id="text" type="password" name="password" placeholder="Enter password here"><br><br>
+  <button id="button"type="submit" value="submit">Login</button>
+  <a href="signup.html">click to Sign Up!</a><br><br>
+</form> 
+</body>
+</html>
